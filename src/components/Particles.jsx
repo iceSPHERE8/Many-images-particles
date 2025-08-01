7d400db2fccaa1284e0955e0c5772584f53a524f
fatoooooony <jacknasppear@gmail.com>
2025-08-01 15:47:23 +0800

    finish the start template

--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -1,17 +1,23 @@
 import { shaderMaterial } from "@react-three/drei";
 import { extend } from "@react-three/fiber";
 
+import vertextShader from "../shaders/particles/vertex.glsl";
+import fragmentShader from "../shaders/particles/fragment.glsl";
+
 const ParticlesShaderMaterial = shaderMaterial(
-    { uTime: 0 },
-    
-)
+  { uTime: 0 },
+  vertextShader,
+  fragmentShader
+);
+
+extend({ ParticlesShaderMaterial });
 
 function Particles() {
   return (
     <>
       <mesh>
         <planeGeometry />
-        <meshBasicMaterial />
+        <particlesShaderMaterial />
       </mesh>
     </>
   );
--- src/shaders/particles/fragment.glsl
+++ src/shaders/particles/fragment.glsl
@@ -3,5 +3,5 @@ uniform float uTime;
 varying vec2 vUv;
 
 void main() {
-    gl_FragColor.rgba = vec4(0.5 + 0.3 * sin(vUv.yxx + time) + vec3, 1.0);
+    gl_FragColor = vec4(vUv, 1.0, 1.0);
 }
